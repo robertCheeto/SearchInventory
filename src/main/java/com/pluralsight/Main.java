bb5198e28ecb4640b2a28785054309f7381ba779
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-08 11:36:21 -0400

    * working on adding ability for user to search for product via its ID

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -10,12 +10,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        ArrayList<Product> inventory = getInventory();
 
-        System.out.println("We have the following items in stock: \n");
-        for (Product product : inventory) {
-            System.out.printf("ID: %d | Item: %s | Price: $%.2f\n", product.getId(), product.getName(), product.getPrice());
-        } // end of for loop
+        displayMenu();
+        int userInput = keyboard.nextInt();
+        keyboard.nextLine();
+        System.out.println("\n*****\nLoading Menu...\n*****\n");
+
+        switch (userInput) {
+            case(1):
+                ArrayList<Product> inventory = getInventory();
+                System.out.println("We have the following items in stock: \n");
+                for (Product product : inventory) {
+                    System.out.printf("ID: %d | Item: %s | Price: $%.2f\n", product.getId(), product.getName(), product.getPrice());
+                } // end of for loop
+            case(2):
+                System.out.print("Enter a Product ID you'd like to search for: ");
+                productSearch();
+
+        }
+
+
     } // end of main()
 
     // https://www.bezkoder.com/java-sort-arraylist-of-objects/ <- good reference for sorting arrays and putting
@@ -49,5 +63,21 @@ public class Main {
 
         return inventory;
     } // end of getInventory
+
+    public static void displayMenu() {
+        System.out.println("*****\nWelcome to Bunnings Mate\n*****");
+        System.out.println("1) - List All Products");
+        System.out.println("2) - Lookup Product by ID");
+        System.out.println("3) - Find All Products within Price Range");
+        System.out.println("4) - Add New Product");
+        System.out.println("5) - Quit the Program");
+        System.out.print("\nPlease enter your choice here: ");
+    } // end of displayMenu()
+
+    public static ArrayList<Product> productSearch() {
+        // wonder if I need to have the array list filled in main for this method to work
+
+
+    } // end of productSearch()
 
 }
