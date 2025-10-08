292a764d8078a07498c18d158064b66e7018dbf8
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-08 11:21:50 -0400

    * was able to sort the list by product name in alphabetical order

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,8 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +16,6 @@ public class Main {
         for (Product product : inventory) {
             System.out.printf("ID: %d | Item: %s | Price: $%.2f\n", product.getId(), product.getName(), product.getPrice());
         } // end of for loop
-
     } // end of main()
 
     // https://www.bezkoder.com/java-sort-arraylist-of-objects/ <- good reference for sorting arrays and putting
@@ -34,12 +35,17 @@ public class Main {
                 float price = Float.parseFloat(parsedList[2]);
 
                 inventory.add(new Product(id, name, price));
-            }
+
+                Collections.sort(inventory, Comparator.comparing(Product::getName));
+                // ask Topher about this function and how it works
+
+            } // end of while loop
             bufReader.close();
+
 
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }
+        } // end of try-catch
 
         return inventory;
     } // end of getInventory
