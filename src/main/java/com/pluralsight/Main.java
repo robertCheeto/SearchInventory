75eee62eefb9d0350bf8e909ab6442441dc0fb14
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-08 10:15:12 -0400

    * figured out how to add inventory items to array list * updated souf formatting for dislpay message

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,13 +6,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        ArrayList<Product> inventory = getInventory();
 
-        System.out.println();
+        System.out.println("We have the following items in stock: ");
+        for (int i = 0; i < inventory.size(); i++) {
+            Product product = inventory.get(i);
+            System.out.printf("ID: %d | Item: %s | Price: $%.2f\n", product.getId(), product.getName(), product.getPrice());
+        } // end of for loop
 
     } // end of main()
 
-    public ArrayList<Product> getInventory() {
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        inventory.add(new Product(001, "Hammer", 2.49f));
+        inventory.add(new Product(002, "Nails", 1.99f));
+        inventory.add(new Product(003, "Screwdriver", 3.29f));
+        inventory.add(new Product(004, "Picture Frame", 5.89f));
+        inventory.add(new Product(005, "Gum - 12 Pack", 0.99f));
 
-    }
+        return inventory;
+
+    } // end of getInventory
 
 }
